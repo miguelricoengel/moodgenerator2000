35cdc325a01a04e71dcc3f77b81e657344b013bc
Mike <ricoengelmiguel@gmail.com>
2023-05-03 21:22:05 +0200

    day1

---
+++ basic-auth/public/js/script.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("basic-auth JS imported successfully!");
+});
+
+
